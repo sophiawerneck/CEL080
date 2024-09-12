dfcf7bdb207517ac49926ffda342a67e72dd6069
Ana Carolina Magalhães <150815052+anacarolmag@users.noreply.github.com>
2024-09-12 11:57:34 -0300

    Update ProjetoFinal.c

--- ProjetoFinal.c
+++ ProjetoFinal.c
@@ -11,10 +11,13 @@
 
 #define EV_TEMP (1 << 0)
 #define EV_UMID (1 << 1)
-#define EV_ALARM_TEMP (1 << 2)
-#define EV_ALARM_UMID (1 << 3)
+#define EV_LUM (1 << 2)
+#define EV_ALARM_TEMP (1 << 3)
+#define EV_ALARM_UMID (1 << 4)
+#define EV_ALARM_LUM (1 << 5)
 
 #define MOISTURE_SENSOR_ADC_CHANNEL ADC1_CHANNEL_6
+#define LDR_ADC_CHANNEL ADC1_CHANNEL_7
 
 struct dht11_reading data;
 
@@ -33,14 +36,17 @@ void app_main(void)
 
     buffer_1 = xMessageBufferCreate(10);
     buffer_2 = xMessageBufferCreate(10);
-    buffer_3 = xMessageBufferCreate(16);
+    buffer_3 = xMessageBufferCreate(10);
     buffer_4 = xMessageBufferCreate(16);
+    buffer_5 = xMessageBufferCreate(16);
+    buffer_6 = xMessageBufferCreate(16);
 
     ev_group = xEventGroupCreate();
 
     xTaskCreate(vTaskLer, "Leitura", 2048, NULL, 3 , NULL);
     xTaskCreate(vTaskMedTemp, "Media_Temp", 2048, NULL, 2 , NULL);
     xTaskCreate(vTaskMedUmid, "Media_Umid", 2048, NULL, 2 , NULL);
+    xTaskCreate(vTaskMedLum, "Media_Lum", 2048, NULL, 2 , NULL);
     xTaskCreate(vTaskDisplay, "Display", 2048, NULL, 1 , NULL);
     xTaskCreate(vTaskAlarme, "Alarme", 2048, NULL, 1 , NULL);
 }
@@ -51,7 +57,9 @@ void vTaskLer(void* pvparameters)
     ESP_LOGI("LEITURA","Task leitura inicializando");
 
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(MOISTURE_SENSOR_ADC_CHANNEL, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(MOISTURE_SENSOR_ADC_CHANNEL, ADC_ATTEN_DB_11); //gpio34 - d34
+
+    adc1_config_channel_atten(LDR_ADC_CHANNEL, ADC_ATTEN_DB_11);
 
     while (1)
     {
@@ -63,10 +71,15 @@ void vTaskLer(void* pvparameters)
         int valor_umidade = adc1_get_raw(MOISTURE_SENSOR_ADC_CHANNEL);
         ESP_LOGE("LEITURA", "Valor do sensor de umidade: %d", valor_umidade);
         xMessageBufferSend(buffer_2, &valor_umidade, sizeof(valor_umidade), portMAX_DELAY);
-        vTaskDelay(pdMS_TO_TICKS(200));
+
+        //Ler luminosidade
+        int valor_lum = adc1_get_raw(LDR_ACD_CHANNEL);
+        ESP_LOGW("LEITURA", "Valor do sensor de luminosidade: %d", valor_umidade);
+        xMessageBufferSend(buffer_3, &valor_lum, sizeof(valor_lum), portMAX_DELAY);
+
+        vTaskDelay(pdMS_TO_TICKS(300));
     }
 }
-
 
 void vTaskMedTemp(void* pvparameters)
 {
@@ -85,7 +98,7 @@ void vTaskMedTemp(void* pvparameters)
         if(cont == 10)
         {
             media1 = soma/10;
-            xMessageBufferSend(buffer_3, &media1, sizeof(media1),portMAX_DELAY);
+            xMessageBufferSend(buffer_4, &media1, sizeof(media1),portMAX_DELAY);
             xEventGroupSetBits(ev_group,EV_TEMP);
             soma = 0;
             cont = 0;
@@ -113,8 +126,35 @@ void vTaskMedUmid(void* pvparameters)
         if(cont == 10)
         {
             media2 = soma/10;
-            xMessageBufferSend(buffer_4, &media2, sizeof(media2),portMAX_DELAY);
+            xMessageBufferSend(buffer_5, &media2, sizeof(media2),portMAX_DELAY);
             xEventGroupSetBits(ev_group,EV_UMID);
+            soma = 0;
+            cont = 0;
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+void vTaskMedLum(void* pvparameters)
+{
+    ESP_LOGI("MEDIA_LUM","Task Media_Lum inicializando");
+
+
+    int luminosidade;
+    float media3;
+    int soma = 0;
+    int cont = 0;
+
+    while(1)
+    {   
+        xMessageBufferReceive(buffer_3, &luminosidade, sizeof(luminosidade),portMAX_DELAY);
+        cont++;
+        soma = soma + luminosidade;
+        if(cont == 10)
+        {
+            media3 = soma/10;
+            xMessageBufferSend(buffer_6, &media3, sizeof(media3),portMAX_DELAY);
+            xEventGroupSetBits(ev_group,EV_LUM);
             soma = 0;
             cont = 0;
         }
@@ -128,27 +168,34 @@ void vTaskDisplay(void* pvparameters)
     EventBits_t bits;
     float temp;
     float umid;
-    float temp_ref = 27.00;
+    float lum;
+    float temp_ref = 30.00;
     float umid_ref = 55.00;
+    float lum_ref = 70.00;
 
     while(1)
     {
-        bits = xEventGroupWaitBits(ev_group, (EV_TEMP | EV_UMID),pdTRUE, pdTRUE,portMAX_DELAY);
+        bits = xEventGroupWaitBits(ev_group, (EV_TEMP | EV_UMID | EV_LUM),pdTRUE, pdTRUE,portMAX_DELAY);
 
-        xMessageBufferReceive(buffer_3, &temp, sizeof(temp), portMAX_DELAY);
-        xMessageBufferReceive(buffer_4, &umid, sizeof(umid), portMAX_DELAY);
+        xMessageBufferReceive(buffer_4, &temp, sizeof(temp), portMAX_DELAY);
+        xMessageBufferReceive(buffer_5, &umid, sizeof(umid), portMAX_DELAY);
+        xMessageBufferReceive(buffer_6, &lum, sizeof(lum), portMAX_DELAY);
         
-        if(bits == (EV_TEMP | EV_UMID)) 
+        if(bits == (EV_TEMP | EV_UMID | EV_LUM)) 
         {
-            ESP_LOGI("DISPLAY", "Média Temperatura: %f, Média Umidade: %f",temp, umid);
+            ESP_LOGI("DISPLAY", "Média Temperatura: %f, Média Umidade: %f, Média Luminosidade: %f",temp, umid, lum);
         }
         if (temp >= temp_ref)
         {
             xEventGroupSetBits(ev_group, EV_ALARM_TEMP); 
         }
-        if (umid >= umid_ref)
+        if (umid <= umid_ref)
         {
             xEventGroupSetBits(ev_group, EV_ALARM_UMID); 
+        } 
+        if (lum >= lum_ref)
+        {
+            xEventGroupSetBits(ev_group, EV_ALARM_LUM); 
         } 
         //vTaskDelay(pdMS_TO_TICKS(500));
     }
@@ -159,14 +206,18 @@ void vTaskAlarme(void* pvparameters)
     EventBits_t bits2;
     while(1)
     {
-        bits2 = xEventGroupWaitBits(ev_group, (EV_ALARM_TEMP | EV_ALARM_UMID), pdTRUE, pdFALSE, portMAX_DELAY);
+        bits2 = xEventGroupWaitBits(ev_group, (EV_ALARM_TEMP | EV_ALARM_UMID | EV_ALARM_LUM), pdTRUE, pdFALSE, portMAX_DELAY);
         if(bits2 & EV_ALARM_TEMP)
         {
             ESP_LOGE("ALARME TEMP", "EXCEDEU A TEMPERATURA");
         }
         if(bits2 & EV_ALARM_UMID)
         {
-            ESP_LOGE("ALARME UMID", "EXCEDEU A UMIDADE");
+            ESP_LOGE("ALARME UMID", "UMIDADE A BAIXO DO LIMITE");
+        }
+        if(bits2 & EV_ALARM_LUM)
+        {
+            ESP_LOGE("ALARME LUM", "EXCEDEU A LUMINOSIDADE");
         }
     }
 }
